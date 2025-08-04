2e2e94d5a83733743ca0fd790a915da0b5c99d2b
Audrey Yang <ayang.penn@gmail.com>
2025-08-03 22:34:32 -0500

    Initial commit of basic app

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import "./App.css";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import images from "./images.json";
+import Pagination from "@mui/material/Pagination";
+import { useState } from "react";
+
+const App = () => {
+  const queryParams = new URLSearchParams(window.location.search);
+  const [page, setPage] = useState(((queryParams.get("p") ?? 0) as number) - 1);
+
+  const handlePageChange = (_: React.ChangeEvent<unknown>, value: number) => {
+    queryParams.set("p", value.toString());
+    window.history.pushState({}, "", `?${queryParams.toString()}`);
+    setPage(value - 1);
+  };
+  const imagesPerPage = 18;
+
+  return (
+    <>
+      <ImageList sx={{ height: "90vh" }} cols={3} rowHeight={150}>
+        {images
+          .slice(imagesPerPage * page, imagesPerPage * (page + 1))
+          .map((name) => {
+            const url = `http://10.0.0.171/images/seattle/${name}`;
+            return (
+              <ImageListItem key={name}>
+                <a href={url} download>
+                  <img
+                    srcSet={url}
+                    src={url}
+                    loading="lazy"
+                    style={{
+                      height: 150,
+                      objectFit: "cover",
+                    }}
+                  />
+                </a>
+              </ImageListItem>
+            );
+          })}
+      </ImageList>
+      <Pagination
+        count={Math.floor(images.length / imagesPerPage) + 1}
+        page={page + 1}
+        onChange={handlePageChange}
+        color="primary"
+      />
+    </>
+  );
+};
+
+export default App;
